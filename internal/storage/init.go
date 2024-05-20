1747fae85621fe33e1c37606e0c4dc46b3c42244
0x726f6f6b6965 <h123453047@icloud.com>
2024-05-21 00:24:53 +0800

    use env password

--- internal/storage/init.go
+++ internal/storage/init.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/0x726f6f6b6965/go-auth/config"
 	"gorm.io/driver/postgres"
@@ -11,7 +12,7 @@ import (
 func NewPostgres(cfg *config.DBConfig) (*gorm.DB, error) {
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%d sslmode=%s",
-		cfg.Host, cfg.User, cfg.Password, cfg.DBName,
+		cfg.Host, cfg.User, os.Getenv(cfg.Password), cfg.DBName,
 		cfg.Port, cfg.SSLmode,
 	)
 	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
